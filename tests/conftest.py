f1c64b3b930694156b6b23aaaf31a34c12ed4474
Stav Lobel <stavlobel@Stavs-MacBook-Air.local>
2025-07-14 18:03:43 +0300

    Keep only global fixtures in root conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
-import os
-from src.pages.login_page import LoginPage
-from src.pages.products_page import ProductsPage
 
 load_dotenv()
 
@@ -22,15 +19,3 @@ def driver():
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
-
-
-@pytest.fixture(scope="function")
-def login_and_go_to_products(driver):
-    username = os.getenv("SAUCE_USERNAME")
-    password = os.getenv("SAUCE_PASSWORD")
-    login_page = LoginPage(driver)
-    login_page.load()
-    login_page.login(username, password)
-    products_page = ProductsPage(driver)
-    assert "inventory" in driver.current_url
-    return products_page
