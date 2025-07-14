3216f6d25f1de6971fe4164b2bd5d085d8c60cfc
Stav Lobel <stavlobel@Stavs-MacBook-Air.local>
2025-07-14 21:21:08 +0300

    Revert "test: comment out all tests in cart edge and persistence suites as requested"
    This reverts commit 38be77f9a1e9614e5b26d26d4186a37e93d094e0.

--- tests/cart/test_cart_persistence.py
+++ tests/cart/test_cart_persistence.py
@@ -16,52 +16,56 @@ ITEMS = [
 ]
 
 
-# @pytest.mark.usefixtures("driver")
-# def test_cart_persistence_after_refresh(login_and_go_to_products):
-#     products_page = login_and_go_to_products
-#     products_page.add_item_by_name(ITEMS[0])
-#     products_page.driver.refresh()
-#     assert products_page.get_cart_count() == 1
-#     products_page.go_to_cart()
-#     cart_page = CartPage(products_page.driver)
-#     assert ITEMS[0] in cart_page.get_cart_items()
+@pytest.mark.usefixtures("driver")
+def test_cart_persistence_after_refresh(login_and_go_to_products):
+    products_page = login_and_go_to_products
+    products_page.add_item_by_name(ITEMS[0])
+    products_page.driver.refresh()
+    assert products_page.get_cart_count() == 1
+    products_page.go_to_cart()
+    cart_page = CartPage(products_page.driver)
+    assert ITEMS[0] in cart_page.get_cart_items()
 
-# @pytest.mark.usefixtures("driver")
-# def test_cart_persistence_after_navigation(login_and_go_to_products):
-#     products_page = login_and_go_to_products
-#     products_page.add_item_by_name(ITEMS[1])
-#     products_page.driver.get("https://www.saucedemo.com/inventory.html")
-#     assert products_page.get_cart_count() == 1
-#     products_page.go_to_cart()
-#     cart_page = CartPage(products_page.driver)
-#     assert ITEMS[1] in cart_page.get_cart_items()
 
-# def test_continue_shopping_from_cart(login_and_go_to_products):
-#     products_page = login_and_go_to_products
-#     products_page.add_item_by_name(ITEMS[0])
-#     products_page.go_to_cart()
-#     cart_page = CartPage(products_page.driver)
-#     cart_page.find(*CartPage.CONTINUE_SHOPPING).click()
-#     assert "inventory" in products_page.driver.current_url
-#     assert products_page.get_cart_count() == 1
+@pytest.mark.usefixtures("driver")
+def test_cart_persistence_after_navigation(login_and_go_to_products):
+    products_page = login_and_go_to_products
+    products_page.add_item_by_name(ITEMS[1])
+    products_page.driver.get("https://www.saucedemo.com/inventory.html")
+    assert products_page.get_cart_count() == 1
+    products_page.go_to_cart()
+    cart_page = CartPage(products_page.driver)
+    assert ITEMS[1] in cart_page.get_cart_items()
 
-# def test_checkout_and_return_to_cart(login_and_go_to_products):
-#     products_page = login_and_go_to_products
-#     products_page.add_item_by_name(ITEMS[0])
-#     products_page.go_to_cart()
-#     cart_page = CartPage(products_page.driver)
-#     cart_page.find(*CartPage.CHECKOUT_BUTTON).click()
-#     # Go back to cart
-#     products_page.driver.get(CartPage.URL)
-#     assert ITEMS[0] in cart_page.get_cart_items()
 
-# def test_cart_persistence_after_clearing_cookies(login_and_go_to_products):
-#     products_page = login_and_go_to_products
-#     products_page.add_item_by_name(ITEMS[0])
-#     products_page.driver.delete_all_cookies()
-#     products_page.driver.refresh()
-#     # Should be logged out or cart should be empty
-#     assert (
-#         products_page.get_cart_count() == 0
-#         or "login" in products_page.driver.current_url
-#     )
+def test_continue_shopping_from_cart(login_and_go_to_products):
+    products_page = login_and_go_to_products
+    products_page.add_item_by_name(ITEMS[0])
+    products_page.go_to_cart()
+    cart_page = CartPage(products_page.driver)
+    cart_page.find(*CartPage.CONTINUE_SHOPPING).click()
+    assert "inventory" in products_page.driver.current_url
+    assert products_page.get_cart_count() == 1
+
+
+def test_checkout_and_return_to_cart(login_and_go_to_products):
+    products_page = login_and_go_to_products
+    products_page.add_item_by_name(ITEMS[0])
+    products_page.go_to_cart()
+    cart_page = CartPage(products_page.driver)
+    cart_page.find(*CartPage.CHECKOUT_BUTTON).click()
+    # Go back to cart
+    products_page.driver.get(CartPage.URL)
+    assert ITEMS[0] in cart_page.get_cart_items()
+
+
+def test_cart_persistence_after_clearing_cookies(login_and_go_to_products):
+    products_page = login_and_go_to_products
+    products_page.add_item_by_name(ITEMS[0])
+    products_page.driver.delete_all_cookies()
+    products_page.driver.refresh()
+    # Should be logged out or cart should be empty
+    assert (
+        products_page.get_cart_count() == 0
+        or "login" in products_page.driver.current_url
+    )
